1db0154617e9b06529fb9132f0bdeb18e5458095
thejackal360 <pariseroman@gmail.com>
2023-11-17 10:32:26 -0800

    Format elena.py and start cleaning up jinja template

--- src/elena/elena.py
+++ src/elena/elena.py
@@ -4,6 +4,7 @@
 
 from flask import Flask, Markup, Response, request, render_template
 from functools import partial
+from itertools import chain
 from jinja2 import Template
 from pathlib import Path
 import json
@@ -29,7 +30,7 @@ def get_http():
     Return which it is.
     @return: string of either "http" or "https"
     """
-    if request.headers.get('X-Forwarded-Proto', 'http') == 'https':
+    if request.headers.get("X-Forwarded-Proto", "http") == "https":
         return "https"
     else:
         return "http"
@@ -52,6 +53,7 @@ def dict_stitching(list_of_dicts):
     assert len(ks) == len(vs)
     return dict(zip(ks, vs))
 
+
 # Core classes
 
 
@@ -69,20 +71,25 @@ class eFlask(Flask):
         super().__init__(**kwargs)
         self.module_list = module_list
         self.domain = domain
-        self.http_post_ptype_to_fn = dict_stitching([
-            m.http_post_ptype_to_fn for m in self.module_list])
-        self.http_get_ptype_to_fn = dict_stitching([
-            m.http_get_ptype_to_fn for m in self.module_list])
-        self.trivia_ptype = ["{}_trivia".format(m.fn_module_name)
-                             for m in self.module_list]
-        self.manual_ptype = ["{}_lab_manual".format(m.fn_module_name)
-                             for m in self.module_list]
+        self.http_post_ptype_to_fn = dict_stitching(
+            [m.http_post_ptype_to_fn for m in self.module_list]
+        )
+        self.http_get_ptype_to_fn = dict_stitching(
+            [m.http_get_ptype_to_fn for m in self.module_list]
+        )
+        self.trivia_ptype = [
+            "{}_trivia".format(m.fn_module_name) for m in self.module_list
+        ]
+        self.manual_ptype = [
+            "{}_lab_manual".format(m.fn_module_name) for m in self.module_list
+        ]
         self.trivia_qs = {}
         for m in self.module_list:
-            with open(Path("static/questions/") / "{}_trivia.json".format(
-                      m.fn_module_name), "r") as t:
-                self.trivia_qs["{}_trivia".format(
-                               m.fn_module_name)] = json.load(t)
+            with open(
+                Path("static/questions/") / "{}_trivia.json".format(m.fn_module_name),
+                "r",
+            ) as t:
+                self.trivia_qs["{}_trivia".format(m.fn_module_name)] = json.load(t)
         self.gen_elenajs()
 
     def eroute(self, rule: str, **options: t.Any) -> t.Callable[[F], F]:
@@ -111,7 +118,9 @@ class eFlask(Flask):
         """
 
         def decorator(f: F) -> F:
-            def _f(): return partial(f, mod=self)()
+            def _f():
+                return partial(f, mod=self)()
+
             endpoint = options.pop("endpoint", None)
             self.add_url_rule(rule, endpoint, _f, **options)
             return _f
@@ -128,9 +137,12 @@ class eFlask(Flask):
                 elena_template_txt = f.read()
             elena_template = Template(elena_template_txt)
             with open(m.elenajs, "w") as f:
-                f.write(elena_template.render(module_name=m.fn_module_name,
-                                              module_name_in_quotes="\""+m.fn_module_name +
-                                              "\""))
+                f.write(
+                    elena_template.render(
+                        module_name=m.fn_module_name,
+                        module_name_in_quotes='"' + m.fn_module_name + '"',
+                    )
+                )
 
     def gen_pick_mod(self):
         """
@@ -138,20 +150,23 @@ class eFlask(Flask):
         one for each module.
         @return: return markup
         """
-        return "\n".join(["\t\t<p class=\"modsel\" " +
-                          " onclick=\"{}_start_lab()\">{}".format(
-                              m.fn_module_name, m.module_name) +
-                          "</p></br></br>"
-                          for m in self.module_list])
+        return "\n".join(
+            [
+                '\t\t<p class="modsel" '
+                + ' onclick="{}_start_lab()">{}'.format(m.fn_module_name, m.module_name)
+                + "</p></br></br>"
+                for m in self.module_list
+            ]
+        )
 
     def handle_requests(self):
         """
         Handle incoming HTTP request
         @return: outgoing HTTP response
         """
-        if request.method == 'POST':
+        if request.method == "POST":
             return self.handle_POST()
-        elif request.method == 'GET':
+        elif request.method == "GET":
             return self.handle_GET()
 
     def handle_POST(self):
@@ -159,10 +174,8 @@ class eFlask(Flask):
         Handle incoming POST request
         @return: the HTTP response
         """
-        if request.headers.get("Internal-Type") in \
-                self.http_post_ptype_to_fn.keys():
-            return self.http_post_ptype_to_fn[
-                request.headers.get('Internal-Type')]()
+        if request.headers.get("Internal-Type") in self.http_post_ptype_to_fn.keys():
+            return self.http_post_ptype_to_fn[request.headers.get("Internal-Type")]()
         else:
             assert False
 
@@ -171,47 +184,56 @@ class eFlask(Flask):
         Handle incoming GET request
         @return: the HTTP response
         """
-        if request.headers.get('Internal-Type') in self.trivia_ptype:
-            qdict = self.trivia_qs[
-                request.headers.get('Internal-Type')]
+        if request.headers.get("Internal-Type") in self.trivia_ptype:
+            qdict = self.trivia_qs[request.headers.get("Internal-Type")]
             q = random.choice(list(qdict.keys()))
-            return Response("[\"{}\",\"{}\"]".format(q, qdict[q]),
-                            mimetype='text/html')
-        elif request.headers.get('Internal-Type') in self.manual_ptype:
-            with open(Path("static/html/") / "{}.html".format(
-                      request.headers.get('Internal-Type')), "r") as f:
+            return Response('["{}","{}"]'.format(q, qdict[q]), mimetype="text/html")
+        elif request.headers.get("Internal-Type") in self.manual_ptype:
+            with open(
+                Path("static/html/")
+                / "{}.html".format(request.headers.get("Internal-Type")),
+                "r",
+            ) as f:
                 return Response(f.read(), mimetype="text/html")
-        elif request.headers.get('Internal-Type') \
-                in self.http_get_ptype_to_fn.keys():
-            return self.http_get_ptype_to_fn[
-                request.headers.get('Internal-Type')]()
+        elif request.headers.get("Internal-Type") in self.http_get_ptype_to_fn.keys():
+            return self.http_get_ptype_to_fn[request.headers.get("Internal-Type")]()
         else:
             http = get_http()
-            gamejs = Markup("\n".join([
-                m.gamejs() for m in self.module_list]))
-            elenajs = Markup("\n".join([
-                m.elenajs_script() for m in self.module_list]))
-            module_js_globals = Markup("\n".join([
-                "\t\t<script src=\"static/js/games/" +
-                "{}_js_globals.js\" ".format(m.fn_module_name) +
-                "type=\"application/javascript\"></script>"
-                for m in self.module_list]))
-            module_subjs = Markup("\n".join([
-                "\t\t<script src=\"static/js/games/" +
-                "{}_subjs.js\" ".format(m.fn_module_name) +
-                "type=\"application/javascript\"></script>"
-                for m in self.module_list]))
-            return render_template('index.html',
-                                   domain=http + "://" + LOCALHOST +
-                                   ":" + str(PORT) + "/"
-                                   if "--local" in sys.argv
-                                   else http + "://" + self.domain,
-                                   gamejs=gamejs,
-                                   elenajs=elenajs,
-                                   module_js_globals=module_js_globals,
-                                   module_subjs=module_subjs,
-                                   pick_mod=Markup(
-                                       self.gen_pick_mod()))
+            js_files = list(
+                chain(*[[g.jsfile for g in m.game_list] for m in self.module_list])
+            )
+            elenajs = Markup("\n".join([m.elenajs_script() for m in self.module_list]))
+            module_js_globals = Markup(
+                "\n".join(
+                    [
+                        '\t\t<script src="static/js/games/'
+                        + '{}_js_globals.js" '.format(m.fn_module_name)
+                        + 'type="application/javascript"></script>'
+                        for m in self.module_list
+                    ]
+                )
+            )
+            module_subjs = Markup(
+                "\n".join(
+                    [
+                        '\t\t<script src="static/js/games/'
+                        + '{}_subjs.js" '.format(m.fn_module_name)
+                        + 'type="application/javascript"></script>'
+                        for m in self.module_list
+                    ]
+                )
+            )
+            return render_template(
+                "index.html",
+                domain=http + "://" + LOCALHOST + ":" + str(PORT) + "/"
+                if "--local" in sys.argv
+                else http + "://" + self.domain,
+                js_files=js_files,
+                elenajs=elenajs,
+                module_js_globals=module_js_globals,
+                module_subjs=module_subjs,
+                pick_mod=Markup(self.gen_pick_mod()),
+            )
 
 
 class Module:
@@ -229,28 +251,35 @@ class Module:
         self.module_name = module_name
         self.fn_module_name = module_name.replace(" ", "_")
         self.game_list = game_list
-        self.elenajs = Path("static/js/games/") / "{}_elena.js".format(
-                       self.module_name)
+        self.elenajs = Path("static/js/games/") / "{}_elena.js".format(self.module_name)
         self.gen_subjs()
         self.gen_jsglobals()
-        self.http_post_ptype_to_fn = dict_stitching([
-            g.http_post_ptype_to_fn for g in self.game_list])
-        self.http_get_ptype_to_fn = dict_stitching([
-            g.http_get_ptype_to_fn for g in self.game_list])
+        self.http_post_ptype_to_fn = dict_stitching(
+            [g.http_post_ptype_to_fn for g in self.game_list]
+        )
+        self.http_get_ptype_to_fn = dict_stitching(
+            [g.http_get_ptype_to_fn for g in self.game_list]
+        )
 
     def gen_subjs(self):
         """
         Generate a subjs file for the module. subjs file contains
         a function for selecting a game.
         """
-        with open("static/js/games/{}_subjs.js".format(
-                self.fn_module_name), "w") as f:
-            f.write("function {}_subjs(topic, val) ".format(
-                self.fn_module_name) + "{\n")
+        with open("static/js/games/{}_subjs.js".format(self.fn_module_name), "w") as f:
+            f.write(
+                "function {}_subjs(topic, val) ".format(self.fn_module_name) + "{\n"
+            )
             f.write("\tswitch (topic) {\n")
-            [f.write("\t\tcase {}{}Topic: {}(val); ".format(
-                     self.fn_module_name, g.gname, g.gname) +
-                     "break;\n") for g in self.game_list]
+            [
+                f.write(
+                    "\t\tcase {}{}Topic: {}(val); ".format(
+                        self.fn_module_name, g.gname, g.gname
+                    )
+                    + "break;\n"
+                )
+                for g in self.game_list
+            ]
             f.write("\t\tdefault: /* do nothing */;\n")
             f.write("\t}\n")
             f.write("}")
@@ -260,31 +289,34 @@ class Module:
         Generate a jsglobals file for the module. jsglobals file
         defines constants for use in selecting games.
         """
-        with open(Path("static/js/games/") / "{}_js_globals.js".format(
-                self.fn_module_name), "w") as f:
-            f.write("const {}{}Topic = 0;\n".format(
-                self.fn_module_name, "Trivia"))
-            [f.write("const {}{}Topic = {};\n".format(
-                     self.fn_module_name, v.gname, i+1))
-             for i, v in enumerate(self.game_list)]
-            f.write("const {}NumTopics = {};".format(
-                self.fn_module_name, len(self.game_list)+1))
+        with open(
+            Path("static/js/games/") / "{}_js_globals.js".format(self.fn_module_name),
+            "w",
+        ) as f:
+            f.write("const {}{}Topic = 0;\n".format(self.fn_module_name, "Trivia"))
+            [
+                f.write(
+                    "const {}{}Topic = {};\n".format(
+                        self.fn_module_name, v.gname, i + 1
+                    )
+                )
+                for i, v in enumerate(self.game_list)
+            ]
+            f.write(
+                "const {}NumTopics = {};".format(
+                    self.fn_module_name, len(self.game_list) + 1
+                )
+            )
 
     def elenajs_script(self):
         """
         Generate HTML markup for importing this module's elena.js
         file.
         """
-        return '\t<script src="{}" '.format(self.elenajs) + \
-               'type="application/javascript"></script>'
-
-    def gamejs(self):
-        """
-        Generate HTML markup for importing each game's elena.js
-        file. Note that each game must be uniquely named. Thus,
-        two different modules cannot have a game of the same name.
-        """
-        return "\n".join([g.get_gamesrc() for g in self.game_list])
+        return (
+            '\t<script src="{}" '.format(self.elenajs)
+            + 'type="application/javascript"></script>'
+        )
 
 
 class Game:
@@ -292,8 +324,7 @@ class Game:
     Class for a game.
     """
 
-    def __init__(self, gname, http_post_ptype_to_fn={},
-                 http_get_ptype_to_fn={}):
+    def __init__(self, gname, http_post_ptype_to_fn={}, http_get_ptype_to_fn={}):
         """
         Initializer.
         @param gname: Game name
@@ -306,11 +337,3 @@ class Game:
         self.jsfile = Path("static/js/games/") / "{}.js".format(self.gname)
         self.http_post_ptype_to_fn = http_post_ptype_to_fn
         self.http_get_ptype_to_fn = http_get_ptype_to_fn
-
-    def get_gamesrc(self):
-        """
-        Generate HTML markup for importing the game's js file.
-        The user must draft the game's js file.
-        """
-        return '\t<script src="{}" '.format(self.jsfile) + \
-               'type="application/javascript"></script>'
